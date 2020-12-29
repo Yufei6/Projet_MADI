749c377427cb8d7685ced9253a2b9f39071cccb9
xiewf2019 <714253971@qq.com>
2020-12-29 23:04:52 +0100

    up

---
+++ main1.py
@@ -0,0 +1,493 @@
+from tkinter import *
+from recherche_policy import *
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+from pl_policy import *
+
+def check_up(cj,li):
+	if li>0:
+		if g[li-1,cj][0]>0 :
+			return True
+	return False
+
+def check_down(cj,li):
+	if li<nblignes-1:
+		if g[li+1,cj][0]>0:
+			return True
+	return False
+
+def check_right(cj,li):
+	if cj<nbcolonnes-1:
+		if g[li,cj+1][0]>0:
+			return True
+	return False
+
+def check_left(cj,li):
+	if cj>0:
+		if g[li,cj-1][0]>0:
+			return True
+	return False
+
+def move_proba_up(cj, li):
+	global PosX,PosY,cost,g
+	cu = check_up(cj,li)
+	cd = check_down(cj,li)
+	cl = check_left(cj,li)
+	cr = check_right(cj,li)
+	if not cl and not cr:
+		PosY -= zoom*20
+		cost[g[li-1,cj,0]]+=g[li-1,cj,1]
+	else:
+		p=np.random.uniform(0,1)
+		if cl and cr:
+			p0 = proba
+			p1 = (1-proba)/2
+			if p < p0:
+				PosY -= zoom*20
+				cost[g[li-1,cj,0]]+=g[li-1,cj,1]
+			elif p < p0+p1:
+				PosX += zoom*20
+				cost[g[li,cj+1,0]]+=g[li,cj+1,1] 
+			else:
+				PosX -= zoom*20
+				cost[g[li,cj-1,0]]+=g[li,cj-1,1] 
+		elif cl and not cr:
+			p0 = (proba+1)/2
+			if p < p0:
+				PosY -= zoom*20
+				cost[g[li-1,cj,0]]+=g[li-1,cj,1]
+			else:
+				PosX -= zoom*20
+				cost[g[li,cj-1,0]]+=g[li,cj-1,1] 
+		elif not cl and cr:
+			p0 = (proba+1)/2
+			if p < p0:
+				PosY -= zoom*20
+				cost[g[li-1,cj,0]]+=g[li-1,cj,1]
+			else:
+				PosX += zoom*20
+				cost[g[li,cj+1,0]]+=g[li,cj+1,1] 
+
+def move_proba_down(cj, li):
+	global PosX,PosY,cost,g
+	cu = check_up(cj,li)
+	cd = check_down(cj,li)
+	cl = check_left(cj,li)
+	cr = check_right(cj,li)
+	if not cl and not cr:
+		PosY += zoom*20
+	else:
+		p=np.random.uniform(0,1)
+		if cl and cr:
+			p0 = proba
+			p1 = (1-proba)/2
+			if p < p0:
+				PosY += zoom*20
+				cost[g[li+1,cj,0]]+=g[li+1,cj,1] 
+			elif p < p0+p1:
+				PosX += zoom*20
+				cost[g[li,cj+1,0]]+=g[li,cj+1,1] 
+			else:
+				PosX -= zoom*20
+				cost[g[li,cj-1,0]]+=g[li,cj-1,1] 
+		elif cl and not cr:
+			p0 = (proba+1)/2
+			if p < p0:
+				PosY += zoom*20
+				cost[g[li+1,cj,0]]+=g[li+1,cj,1] 
+			else:
+				PosX -= zoom*20
+				cost[g[li,cj-1,0]]+=g[li,cj-1,1] 
+		elif not cl and cr:
+			p0 = (proba+1)/2
+			if p < p0:
+				PosY += zoom*20
+				cost[g[li+1,cj,0]]+=g[li+1,cj,1] 
+			else:
+				PosX += zoom*20
+				cost[g[li,cj+1,0]]+=g[li,cj+1,1] 
+
+def move_proba_right(cj, li):
+	global PosX,PosY,cost,g
+	cu = check_up(cj,li)
+	cd = check_down(cj,li)
+	cl = check_left(cj,li)
+	cr = check_right(cj,li)
+	if not cu and not cd:
+		PosX += zoom*20
+	else:
+		p=np.random.uniform(0,1)
+		if cu and cd:
+			p0 = proba
+			p1 = (1-proba)/2
+			if p < p0:
+				PosX += zoom*20
+				cost[g[li,cj+1,0]]+=g[li,cj+1,1] 
+			elif p < p0+p1:
+				PosY += zoom*20
+				cost[g[li+1,cj,0]]+=g[li+1,cj,1] 
+			else:
+				PosY -= zoom*20
+				cost[g[li-1,cj,0]]+=g[li-1,cj,1]
+		elif cu and not cd:
+			p0 = (proba+1)/2
+			if p < p0:
+				PosX += zoom*20
+				cost[g[li,cj+1,0]]+=g[li,cj+1,1] 
+			else:
+				PosY -= zoom*20
+				cost[g[li-1,cj,0]]+=g[li-1,cj,1]
+		elif not cu and cd:
+			p0 = (proba+1)/2
+			if p < p0:
+				PosX += zoom*20
+				cost[g[li,cj+1,0]]+=g[li,cj+1,1] 
+			else:
+				PosY += zoom*20
+				cost[g[li+1,cj,0]]+=g[li+1,cj,1] 
+
+def move_proba_left(cj, li):
+	global PosX,PosY,cost,g
+	cu = check_up(cj,li)
+	cd = check_down(cj,li)
+	cl = check_left(cj,li)
+	cr = check_right(cj,li)
+	if not cu and not cd:
+		PosX -= zoom*20
+	else:
+		p=np.random.uniform(0,1)
+		if cu and cd:
+			p0 = proba
+			p1 = (1-proba)/2
+			if p < p0:
+				PosX -= zoom*20
+				cost[g[li,cj-1,0]]+=g[li,cj-1,1] 
+			elif p < p0+p1:
+				PosY += zoom*20
+				cost[g[li+1,cj,0]]+=g[li+1,cj,1] 
+			else:
+				PosY -= zoom*20
+				cost[g[li-1,cj,0]]+=g[li-1,cj,1]
+		elif cu and not cd:
+			p0 = (proba+1)/2
+			if p < p0:
+				PosX -= zoom*20
+				cost[g[li,cj-1,0]]+=g[li,cj-1,1] 
+			else:
+				PosY -= zoom*20
+				cost[g[li-1,cj,0]]+=g[li-1,cj,1]
+		elif not cu and cd:
+			p0 = (proba+1)/2
+			if p < p0:
+				PosX -= zoom*20
+				cost[g[li,cj-1,0]]+=g[li,cj-1,1] 
+			else:
+				PosY += zoom*20
+				cost[g[li+1,cj,0]]+=g[li+1,cj,1] 
+
+def initialize():
+	global PosX,PosY,cost
+# position initiale du robot
+	PosX = 20+10*zoom
+	PosY = 20+10*zoom
+	for k in range(6):
+		cost[k]=0
+# cout et affichage
+	Canevas.coords(Pion,PosX -9*zoom, PosY -9*zoom, PosX +9*zoom, PosY +9*zoom)
+	wg.config(text=str(cost[1]))
+	wb.config(text=str(cost[2]))
+	wr.config(text=str(cost[3]))
+	wn.config(text=str(cost[4]))
+	ws.config(text='     total = '+str(cost[0]))
+
+def display_policy():
+	window = Tk()
+	window.title("Politique Total")
+	cv = Canvas(window, width = Largeur, height =Hauteur, bg =mywhite)
+	for i in range(nblignes):
+		for j in range(nbcolonnes):          
+			y =zoom*20*i+20
+			x =zoom*20*j+20
+			if g[i,j,0]>0:       
+				#Canevas.create_oval(x+zoom*(10-3),y+zoom*(10-3),x+zoom*(10+3),y+zoom*(10+3),width=1,outline=color[g[i,j]],fill=color[g[i,j]])
+				if policy[i][j]==0:
+					action_policy = '↑'
+				elif policy[i][j]==1:
+					action_policy = '↓'
+				elif policy[i][j]==2:
+					action_policy = '←'
+				elif policy[i][j]==3:
+					action_policy = '→'
+				cv.create_text(x+zoom*(10),y+zoom*(10), text=action_policy,fill=mygreen,font = "Verdana "+str(int(6*zoom))+" bold")
+			else:
+				cv.create_rectangle(x, y, x+zoom*20, y+zoom*20, fill=mywalls)
+	for i in range(nblignes+2):
+		ni=zoom*20*i+20
+		cv.create_line(20, ni, Largeur-20,ni)
+	for j in range(nbcolonnes+2):
+		nj=zoom*20*j+20
+		cv.create_line(nj, 20, nj, Hauteur-20)
+	cv.focus_set()
+	cv.pack(padx =5, pady =5)
+	window.mainloop()
+
+
+	
+
+
+def Clavier(event):
+	global PosX,PosY
+	touche = event.keysym
+	cj=round((PosX-30)/(20*zoom))
+	li=round((PosY-30)/(20*zoom))
+	# print(li,cj)
+	# deplacement vers le haut
+	if touche == 'Up' and check_up(cj,li):
+		move_proba_up(cj, li)
+	# deplacement vers le bas
+	if touche == 'Down' and check_down(cj,li):
+		move_proba_down(cj, li)
+	# deplacement vers la droite
+	if touche == 'Right' and check_right(cj,li):
+		move_proba_right(cj, li)
+	# deplacement vers la gauche
+	if touche == 'Left' and check_left(cj,li):
+		move_proba_left(cj, li)
+	if touche == 'Space':
+		if policy[cj,li]==0:
+			move_proba_up(cj, li)
+		elif policy[cj,li]==1:
+			move_proba_down(cj, li)
+		elif policy[cj,li]==2:
+			move_proba_left(cj, li)
+		elif policy[cj,li]==3:
+			move_proba_right(cj, li)
+	# on dessine le pion a sa nouvelle position
+	Canevas.coords(Pion,PosX -9*zoom, PosY -9*zoom, PosX +9*zoom, PosY +9*zoom)
+	cost[0]=0    
+	for k in range(4):
+		cost[0]+=cost[k+1]*weight[k+1]        
+	wg.config(text=str(cost[1]))
+	wb.config(text=str(cost[2]))
+	wr.config(text=str(cost[3]))
+	wn.config(text=str(cost[4]))
+	ws.config(text='     total = '+str(cost[0]))
+
+
+def colordraw(g,nblignes,nbcolonnes, _display=True):
+	pblanc=0.1
+	pverte=0.3
+	pbleue=0.25
+	prouge=0.2
+	pnoire=0.15
+	for i in range(nblignes):
+		for j in range(nbcolonnes):
+			y =20*i+20
+			x =20*j+20
+			z=np.random.uniform(0,1)
+			if z < pblanc:
+				c=0
+			else:
+				if z < pblanc+ pverte:
+					c=1
+				else:
+					if z < pblanc+ pverte + pbleue:
+						c=2
+					else:
+						if z< pblanc+ pverte + pbleue +prouge:
+							c=3
+						else:
+							c=4            
+			if c>0:
+				g[i,j,0]=c
+				g[i,j,1]=np.random.randint(1, 9 + 1)
+	g[0,0,0]=np.random.randint(1, 3 + 1)
+	g[0,1,0]=np.random.randint(1, 3 + 1)
+	g[2,0,0]=np.random.randint(1, 3 + 1)
+	g[nblignes-1,nbcolonnes-1]=np.random.randint(1, 3 + 1)
+	g[nblignes-2,nbcolonnes-1]=np.random.randint(1, 3 + 1)
+	g[nblignes-1,nbcolonnes-2]=np.random.randint(1, 3 + 1)
+	if _display:
+		for i in range(nblignes):
+			for j in range(nbcolonnes):          
+				y =zoom*20*i+20
+				x =zoom*20*j+20
+				if g[i,j,0]>0:            
+					#Canevas.create_oval(x+zoom*(10-3),y+zoom*(10-3),x+zoom*(10+3),y+zoom*(10+3),width=1,outline=color[g[i,j]],fill=color[g[i,j]])
+					Canevas.create_text(x+zoom*(10),y+zoom*(10), text=str(g[i,j,1]),fill=color[g[i,j,0]],font = "Verdana "+str(int(6*zoom))+" bold")
+				else:
+					Canevas.create_rectangle(x, y, x+zoom*20, y+zoom*20, fill=mywalls)
+	set_objectif(g,nblignes, nbcolonnes, 1000, _display)
+	#print("GGG",g)
+
+def set_objectif(g, nblignes,nbcolonnes, _value_objectif, _display):
+	global value_objectif
+	value_objectif = _value_objectif
+	i = nblignes-1
+	j = nbcolonnes-1
+	g[i, j, 0] = 5
+	g[i, j, 1] = value_objectif
+	if _display:
+		y =zoom*20*i+20
+		x =zoom*20*j+20
+		Canevas.create_rectangle(x, y, x+zoom*20, y+zoom*20,  fill=myred)
+
+
+
+def set_parameters(nbligness , nbcolonness, probas, weights):
+	global nblignes , nbcolonnes, proba, weight
+	nblignes = nbligness
+	nbcolonnes = nbcolonness
+	proba = probas
+	weight = np.zeros(6, dtype=np.int)
+	weight[1] = weights[1]
+	weight[2] = weights[2]
+	weight[3] = weights[3]
+	weight[4] = weights[4]
+	weight[5] = weights[5]
+
+
+def init_game(_nblignes , _nbcolonness, _proba, _weight, _zoom=2, _PosX=20, _PosY=20, _gamma=0.9, _display=True, _q=1):
+	global g, cost, Pion, zoom, PosX, PosY, Canevas, policy, Largeur, Hauteur, times_list, iterations_list
+	global color, myred, mygreen, myblue, mygrey, myyellow, myblack, mywalls, mywhite, w, wg, wb, wr, wn, ws
+
+	set_parameters(_nblignes , _nbcolonness, _proba, _weight)
+	gamma = _gamma
+	zoom = _zoom
+	PosX = _PosX
+	PosY = _PosY
+
+	if _display:
+		Mafenetre = Tk()
+		Mafenetre.title('MADI PROJET')
+		# position initiale du pion
+		PosX = 20+10*zoom
+		PosY = 20+10*zoom
+
+		# Creation d'un widget Canvas (zone graphique)
+		Largeur = zoom*20*nbcolonnes+40
+		Hauteur = zoom*20*nblignes+40
+		 
+		# valeurs de la grille
+		g = np.zeros((nblignes,nbcolonnes,2), dtype=np.int)
+		cost= np.zeros(6, dtype=np.int)
+
+		
+
+		myred="#F70B42"
+		mygreen="#1AD22C"
+		myblue="#0B79F7"
+		mygrey="#E8E8EB"
+		myyellow="#F9FB70"
+		myblack="#5E5E64"
+		mywalls="#5E5E64"
+		mywhite="#FFFFFF"
+
+		color=[mywhite,mygreen,myblue,myred,myblack]
+
+		Canevas = Canvas(Mafenetre, width = Largeur, height =Hauteur, bg =mywhite)
+		for i in range(nblignes+2):
+			ni=zoom*20*i+20
+			Canevas.create_line(20, ni, Largeur-20,ni)
+		for j in range(nbcolonnes+2):
+			nj=zoom*20*j+20
+			Canevas.create_line(nj, 20, nj, Hauteur-20)
+		colordraw(g,nblignes,nbcolonnes)
+		Pion = Canevas.create_oval(PosX -9*zoom, PosY -9*zoom, PosX +9*zoom, PosY +9*zoom,width=2,outline='black',fill=myyellow)
+		Canevas.focus_set()
+		Canevas.bind('<Key>',Clavier)
+		Canevas.pack(padx =5, pady =5)
+
+
+		policy, iteration = itervalue(g, nblignes, nbcolonnes, proba, gamma , e=0.0001, objectif=value_objectif, _q=_q)
+		g1=change_grill(g,nblignes, nbcolonnes,value_objectif)
+		policy=optimalepure(nblignes, nbcolonnes,4 , g1,proba, gamma)
+		print("aaa",policy)
+
+		# Craation d'un widget Button (bouton Quitter)
+		# Creation d'un widget Button (bouton Quitter)
+		Button(Mafenetre, text ='Restart', command = initialize).pack(side=LEFT,padx=5,pady=5)
+		Button(Mafenetre, text ='Quit', command = Mafenetre.destroy).pack(side=LEFT,padx=5,pady=5)
+		Button(Mafenetre, text ='Policy', command = display_policy).pack(side=LEFT,padx=5,pady=5)
+
+		w = Label(Mafenetre, text='     Costs: ', fg=myblack,font = "Verdana "+str(int(5*zoom))+" bold")
+		w.pack(side=LEFT,padx=5,pady=5) 
+		wg = Label(Mafenetre, text=str(cost[1]),fg=mygreen,font = "Verdana "+str(int(5*zoom))+" bold")
+		wg.pack(side=LEFT,padx=5,pady=5) 
+		wb = Label(Mafenetre, text=str(cost[2]),fg=myblue,font = "Verdana "+str(int(5*zoom))+" bold")
+		wb.pack(side=LEFT,padx=5,pady=5) 
+		wr = Label(Mafenetre, text=str(cost[3]),fg=myred,font = "Verdana "+str(int(5*zoom))+" bold")
+		wr.pack(side=LEFT,padx=5,pady=5) 
+		wn = Label(Mafenetre, text=str(cost[4]),fg=myblack,font = "Verdana "+str(int(5*zoom))+" bold")
+		wn.pack(side=LEFT,padx=5,pady=5) 
+		ws = Label(Mafenetre, text='     total = '+str(cost[0]),fg=myblack,font = "Verdana "+str(int(5*zoom))+" bold")
+		ws.pack(side=LEFT,padx=5,pady=5) 
+		Mafenetre.mainloop()
+
+	else:
+		# valeurs de la grille
+		g = np.zeros((nblignes,nbcolonnes,2), dtype=np.int)
+		cost= np.zeros(6, dtype=np.int)
+		colordraw(g,nblignes,nbcolonnes,_display)
+		t0 = time.time()
+		policy, iteration = itervalue(g, nblignes, nbcolonnes, proba, gamma , e=0.0001, objectif=value_objectif)
+		t1 = time.time()
+		times_list.append(t1-t0)
+		iterations_list.append(iteration)
+
+def comparer_make_images():
+	global times_list, iterations_list
+	_weight = [0,1,2,3,4,-1]
+
+	times_list=[]
+	iterations_list=[]
+	times_mean=[]
+	iterations_mean=[]
+	for p in [1,0.6]:
+		for nbl,nbc in [[10,10],[10,15],[15,20]]:
+			y_time = []
+			y_iter = []
+			for g in [0.9,0.7,0.5]:
+				times_list=[]
+				iterations_list=[]
+				for i in range(15):
+					init_game(nbl , nbc, _proba=p, _weight=_weight, _gamma=g, _display=False)
+				times_mean.append(np.mean(np.array(times_list)))
+				iterations_mean.append(np.mean(np.array(iterations_list)))
+				y_time.append(np.mean(np.array(times_list)))
+				y_iter.append(np.mean(np.array(iterations_list)))
+			x = [0.9,0.7,0.5]
+			ln1, = plt.plot(x, y_time, color='red')
+			plt.legend(handles=[ln1],labels=['time mean'])
+			plt.title("Moyenne de temps")
+			plt.savefig('./Results/'+str(p)+"_"+str(nbl)+"mult"+str(nbc)+"_time.jpg")
+			plt.xlabel("Different gamma")
+			plt.ylabel("Time mean")
+			plt.show()
+			ln2, = plt.plot(x, y_iter, color='blue')
+			plt.legend(handles=[ln2],labels=['iteration mean'])
+			plt.title("Moyenne d'itérations")
+			plt.savefig('./Results/'+str(p)+"_"+str(nbl)+"mult"+str(nbc)+"_iter.jpg")
+			plt.xlabel("Different gamma")
+			plt.ylabel("Iteration mean")
+			plt.show()
+
+if __name__ == "__main__":
+
+	#question 2b
+	#comparer_make_images()
+
+	#question 2c
+	_nblignes = 10
+	_nbcolonness = 15
+	_proba = 0.8
+	_weight = [0,1,2,3,4,-1]
+	_gamma = 0.9
+	_display = True
+	_q = 1
+
+	init_game(_nblignes , _nbcolonness, _proba=_proba, _weight=_weight, _gamma=_gamma, _display=_display, _q=_q)
+
+	
+
--- pl_policy.py
+++ pl_policy.py
@@ -1,12 +1,11 @@
 import gurobipy as gb
 from gurobipy import *
+import numpy as np
 
 def calculTransfert(grill,i,j,a,p):
     
-    c=i
-    d=j
-    i-=1
-    j-=1
+    c=i+1
+    d=j+1
     p1=(1+p)/2
     p2=(1-p)/2
     if a==0:#up
@@ -65,16 +64,14 @@ def calculTransfert(grill,i,j,a,p):
             
     return up,down,left,right,reset
 
-def calculR(grill, i,j,a,p):
-    c=i
-    d=j
-    i-=1
-    j-=1
+def calculR(grill, i,j,ax,p):
+    c=i+1
+    d=j+1
     p1=(1+p)/2
     p2=(1-p)/2
-    if a==0:#up
+    if ax==0:#up
         if(grill[c-1][d][0]==0):
-            a=-gril[i][j]
+            a=-grill[i][j][0]
         elif grill[c][d-1][0]==0 and grill[c][d+1][0]==0:
             a= -grill[c-1][d][0]
         elif grill[c][d-1][0]==0 and grill[c][d+1][0]!=0:
@@ -83,9 +80,9 @@ def calculR(grill, i,j,a,p):
             a=-grill[c-1][d][0]*p1-grill[c][d-1][0]*p2
         else: 
             a=-grill[c-1][d][0]*p-grill[c][d-1][0]*p2+grill[c][d+1][0]*p2
-    if a==1:#down
+    if ax==1:#down
         if(grill[c+1][d][0]==0):
-            a=-gril[i][j]
+            a=-grill[i][j][0]
         elif grill[c][d-1][0]==0 and grill[c][d+1][0]==0:
             a= -grill[c+1][d][0]
         elif grill[c][d-1][0]==0 and grill[c][d+1][0]!=0:
@@ -94,10 +91,10 @@ def calculR(grill, i,j,a,p):
             a=-grill[c+1][d][0]*p1-grill[c][d-1][0]*p2
         else: 
             a=-grill[c+1][d][0]*p-grill[c][d-1][0]*p2+grill[c][d+1][0]*p2
-    if a==2:#left
+    if ax==2:#left
         if(grill[c][d-1][0]==0):
             
-            a=-gril[i][j]
+            a=-grill[i][j][0]
         elif grill[c+1][d][0]==0 and grill[c-1][d][0]==0:
             
             a= -grill[c][d-1][0]
@@ -111,10 +108,10 @@ def calculR(grill, i,j,a,p):
             
             a=-grill[c][d-1][0]*p-grill[c+1][d][0]*p2+grill[c-1][d][0]*p2
         
-    if a==3:#right
+    if ax==3:#right
         if(grill[c][d+1][0]==0):
             
-            a=-gril[i][j]
+            a=-grill[i][j][0]
         elif grill[c+1][d][0]==0 and grill[c-1][d][0]==0:
             
             a= -grill[c][d+1][0]
@@ -129,6 +126,7 @@ def calculR(grill, i,j,a,p):
             a=-grill[c][d+1][0]*p-grill[c+1][d][0]*p2+grill[c-1][d][0]*p2
     return a
 
+
 def optimale(n,m,nba,grill,p,gamma):
     
     # G : matrice des gains
@@ -145,22 +143,31 @@ def optimale(n,m,nba,grill,p,gamma):
     G=[]
     for i in range(n):
         for j in range(m):
-            tab==np.zeros(n*m*j)
+            tab=np.zeros(n*m*nba)
             for a in range(nba):
                 tab[i*m*nba+j*nba+a]+=1
-                up,down,left,right,reset=calculTransfert(grill,i,j,a,p)
-                tab[i*m*nba+j*nba+a]+=reset*gamma
-                tab[(i-1)*m*nba+j*nba+a]+=up*gamma
-                tab[(i+1)*m*nba+j*nba+a]+=down*gamma
-                tab[i*m*nba+(j-1)*nba+a]+=left*gamma
-                tab[i*m*nba+(j-1)*nba+a]+=right*gamma
-                tab[i*m*nba+(j-1)*nba+a]+=right*gamma
+                if(grill[i+1][j+1][0]!=0):
+                    up,down,left,right,reset=calculTransfert(grill,i,j,a,p)
+                    tab[i*m*nba+j*nba+a]+=reset*gamma
+                    if(i>0):
+                        tab[(i-1)*m*nba+j*nba+a]+=up*gamma
+                    if(i<n-1):
+                        tab[(i+1)*m*nba+j*nba+a]+=down*gamma
+                    if(j>0):
+                        tab[i*m*nba+(j-1)*nba+a]+=left*gamma
+                    if(j<n-1):  
+                        tab[i*m*nba+(j-1)*nba+a]+=right*gamma
             G.append(tab)
     G=np.array(G)
     # Second membre
     b = []
-    for i in range(n*m) :
-        b.append(1)
+    
+    for i in range(n) :
+        for j in range(m):
+            if(grill[i+1][j+1][0]==0):
+                b.append(0)
+            else:
+                b.append(1)
     # Coefficients de la fonction objectif
     c = []
     for i in range(n) :
@@ -170,15 +177,15 @@ def optimale(n,m,nba,grill,p,gamma):
                 c.append(r)
 
          
-    m = Model("mogplex")     
+    m1 = Model("mogplex")     
 
     # declaration variables de decision
     x = []
     for i in colonnes:
-        x.append(m.addVar(vtype=GRB.CONTINUOUS, lb=0, name="x%d" % (i+1)))
+        x.append(m1.addVar(vtype=GRB.CONTINUOUS, lb=0, name="x%d" % (i+1)))
 
     # maj du modele pour integrer les nouvelles variables
-    m.update()
+    m1.update()
 
     obj = LinExpr();
     obj =0
@@ -186,14 +193,14 @@ def optimale(n,m,nba,grill,p,gamma):
         obj += c[j] * x[j]
 
     # definition de l'objectif
-    m.setObjective(obj,GRB.MINIMIZE)
-
+    m1.setObjective(obj,GRB.MAXIMIZE)
+    m1.setParam("OutputFlag",False)
     # Definition des contraintes
     for i in lignes:
-        m.addConstr(quicksum(G[i][j]*x[j] for j in colonnes) == b[i], "Contrainte%d" % i)
+        m1.addConstr(quicksum(G[i][j]*x[j] for j in colonnes) == b[i], "Contrainte%d" % i)
 
     # Resolution
-    m.optimize()
+    m1.optimize()
 
 
     # print("")                
@@ -202,83 +209,23 @@ def optimale(n,m,nba,grill,p,gamma):
     # print("")
     # print('Valeur de la fonction objectif :', m.objVal)
     # print("dico :",dico_opt)
-    
-    return x
-def optimale(n,m,nba,grill,p,gamma):
-    
-    # G : matrice des gains
-   
-
-    nbcont = n*m
-    nbvar = n*m*nba
-
-    # Range of plants and warehouses
-    lignes = range(nbcont)
-    colonnes = range(nbvar)
-
-    # Matrice des contraintes
-    G=[]
+    tab=[]
     for i in range(n):
         for j in range(m):
-            tab==np.zeros(n*m*j)
+            tab1=[]
             for a in range(nba):
-                tab[i*m*nba+j*nba+a]+=1
-                up,down,left,right,reset=calculTransfert(grill,i,j,a,p)
-                tab[i*m*nba+j*nba+a]+=reset*gamma
-                tab[(i-1)*m*nba+j*nba+a]+=up*gamma
-                tab[(i+1)*m*nba+j*nba+a]+=down*gamma
-                tab[i*m*nba+(j-1)*nba+a]+=left*gamma
-                tab[i*m*nba+(j-1)*nba+a]+=right*gamma
-            G.append(tab)
-    G=np.array(G)
-    # Second membre
-    b = []
+                
+                v=x[i*m*nba+j*nba+a].X
+                tab1.append(v)
+            amax=np.argmax(tab1)
+            v=tab1[amax]
+            if (v>0):
+                for el in tab1:
+                    if el >0 and el!=v:
+                        print(tab1)   
+            tab.append(tab1)
     
-    for i in range(n*m) :
-        b.append(1)
-    # Coefficients de la fonction objectif
-    c = []
-    for i in range(n) :
-        for j in range(m):
-            for a in range(nba):
-                r=calculR(grill, i,j,a,p)
-                c.append(r)
-
-         
-    m = Model("mogplex")     
-
-    # declaration variables de decision
-    x = []
-    for i in colonnes:
-        x.append(m.addVar(vtype=GRB.CONTINUOUS, lb=0, name="x%d" % (i+1)))
-
-    # maj du modele pour integrer les nouvelles variables
-    m.update()
-
-    obj = LinExpr();
-    obj =0
-    for j in colonnes:
-        obj += c[j] * x[j]
-
-    # definition de l'objectif
-    m.setObjective(obj,GRB.MINIMIZE)
-
-    # Definition des contraintes
-    for i in lignes:
-        m.addConstr(quicksum(G[i][j]*x[j] for j in colonnes) == b[i], "Contrainte%d" % i)
-
-    # Resolution
-    m.optimize()
-
-
-    # print("")                
-    # print('Solution optimale:')
-    
-    # print("")
-    # print('Valeur de la fonction objectif :', m.objVal)
-    # print("dico :",dico_opt)
-    
-    return x
+    return tab
 
 def optimalepure(n,m,nba,grill,p,gamma):
     
@@ -296,45 +243,56 @@ def optimalepure(n,m,nba,grill,p,gamma):
     G=[]
     for i in range(n):
         for j in range(m):
-            tab==np.zeros(n*m*j)
+            tab=np.zeros(n*m*nba)
             for a in range(nba):
                 tab[i*m*nba+j*nba+a]+=1
-                up,down,left,right,reset=calculTransfert(grill,i,j,a,p)
-                tab[i*m*nba+j*nba+a]+=reset*gamma
-                tab[(i-1)*m*nba+j*nba+a]+=up*gamma
-                tab[(i+1)*m*nba+j*nba+a]+=down*gamma
-                tab[i*m*nba+(j-1)*nba+a]+=left*gamma
-                tab[i*m*nba+(j-1)*nba+a]+=right*gamma
+                if(grill[i+1][j+1][0]!=0):
+                    up,down,left,right,reset=calculTransfert(grill,i,j,a,p)
+                    tab[i*m*nba+j*nba+a]+=reset*gamma
+                    if(i>0):
+                        tab[(i-1)*m*nba+j*nba+a]+=up*gamma
+                    if(i<n-1):
+                        tab[(i+1)*m*nba+j*nba+a]+=down*gamma
+                    if(j>0):
+                        tab[i*m*nba+(j-1)*nba+a]+=left*gamma
+                    if(j<n-1):  
+                        tab[i*m*nba+(j-1)*nba+a]+=right*gamma
             G.append(tab)
     G=np.array(G)
     # Second membre
     b = []
     
-    for i in range(n*m) :
-        b.append(1)
+    for i in range(n) :
+        for j in range(m):
+            if(grill[i+1][j+1][0]==0):
+                b.append(0)
+            else:
+                b.append(1)
     # Coefficients de la fonction objectif
     c = []
     for i in range(n) :
         for j in range(m):
             for a in range(nba):
+                print(a)
                 r=calculR(grill, i,j,a,p)
                 c.append(r)
 
          
-    m = Model("mogplex")     
+    m1 = Model("mogplex")     
 
     # declaration variables de decision
     x = []
     for i in colonnes:
-        x.append(m.addVar(vtype=GRB.CONTINUOUS, lb=0, name="x%d" % (i+1)))
+        x.append(m1.addVar(vtype=GRB.CONTINUOUS, lb=0, name="x%d" % (i+1)))
     d=[]
     tab=[]
     for i in range(n):
-        for j in range(m):  
-            d.append(m.addVar(vtype=GRB.BINARY, lb=0,ub=1))
+        for j in range(m): 
+            for a in range(nba): 
+                d.append(m1.addVar(vtype=GRB.BINARY, lb=0,ub=1))
 
     # maj du modele pour integrer les nouvelles variables
-    m.update()
+    m1.update()
 
     obj = LinExpr();
     obj =0
@@ -342,19 +300,19 @@ def optimalepure(n,m,nba,grill,p,gamma):
         obj += c[j] * x[j]
 
     # definition de l'objectif
-    m.setObjective(obj,GRB.MINIMIZE)
+    m1.setObjective(obj,GRB.MAXIMIZE)
 
     # Definition des contraintes
     for i in lignes:
-        m.addConstr(quicksum(G[i][j]*x[j] for j in colonnes) == b[i], "Contrainte%d" % i)
+        m1.addConstr(quicksum(G[i][j]*x[j] for j in colonnes) == b[i], "Contrainte%d" % i)
     for i in range(n):
         for j in range(m):
-            m.addConstr(quicksum(d[i*m+j*nba+a] for a in range(nba)) <= 1, "Contrainte%d" % i*m+j+nbcont)
+            m1.addConstr(quicksum(d[i*m*nba+j*nba+a] for a in range(nba)) <= 1, "Contrainte%d" % (i*m+j+nbcont))
     for i in colonnes:
-        m.addConstr((1-gamma)*x[i]<= d[i], "Contrainte%d" % i+n*m+nbcont)
+        m1.addConstr((1-gamma)*x[i]<= d[i], "Contrainte%d" % (i+n*m+nbcont))
 
     # Resolution
-    m.optimize()
+    m1.optimize()
 
 
     # print("")                
@@ -363,5 +321,20 @@ def optimalepure(n,m,nba,grill,p,gamma):
     # print("")
     # print('Valeur de la fonction objectif :', m.objVal)
     # print("dico :",dico_opt)
-    
-    return x
+    tab=np.zeros((n,m))
+    for i in range(n):
+        for j in range(m):
+            tab1=[]
+            for a in range(nba):
+                
+                v=x[i*m*nba+j*nba+a].X
+                tab1.append(v)
+            amax=np.argmax(tab1)
+            v=tab1[amax]
+            if (v>0):
+                for el in tab1:
+                    if el >0 and el!=v:
+                        print("error")   
+            tab[i][j]=amax
+
+    return tab
