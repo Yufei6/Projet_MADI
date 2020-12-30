dc9e44c335d57a27ec76baaf01834f1a265ab053
xiewf2019 <714253971@qq.com>
2020-12-30 16:42:34 +0100

    Create pl_policy.py

---
+++ changeif/pl_policy.py
@@ -0,0 +1,380 @@
+import gurobipy as gb
+from gurobipy import *
+import numpy as np
+def transfer(a):
+    if a==0:
+        return(-1,0)
+    if a==1:
+        return(1,0)
+    if a==2:
+        return(0,-1)
+    if a==3:
+        return(0,1)
+
+def calculTransfert(grill,i,j,a,p):
+    
+    c=i+1
+    d=j+1
+    p1=(1+p)/2
+    p2=(1-p)/2
+    e,f=transfer(a)
+    up,down,left,right,reset，upleft,upright,downleft,downright=0,0,0,0,0,0,0,0,0
+    if a==0:#up
+        if grill[c-1][d][0]==0 :
+            reset=1
+        if grill[c][d-1][0]==0 and grill[c][d+1][0]==0:
+            down=1
+        if grill[c][d-1][0]!=0 and grill[c][d+1][0]==0:
+            down=p1
+        if grill[c][d-1][0]==0 and grill[c][d+1][0]!=0:
+            down=p1
+        if grill[c][d-1][0]!=0 and grill[c][d+1][0]!=0:
+            down= p
+        if grill[c][d-1][0]!=0 :
+            downleft=p2 
+        if grill[c][d+1][0]!=0:           
+            downright=p2 
+    if a==1:#down
+        if grill[c+1][d][0]==0 :
+            reset=1
+        if grill[c][d-1][0]==0 and grill[c][d+1][0]==0:
+            up=1
+        if grill[c][d-1][0]!=0 and grill[c][d+1][0]==0:
+            up=p1
+        if grill[c][d-1][0]==0 and grill[c][d+1][0]!=0:
+            up=p1
+        if grill[c][d-1][0]!=0 and grill[c][d+1][0]!=0:
+            up= p
+        if grill[c][d-1][0]!=0 :
+            upleft=p2 
+        if grill[c][d+1][0]!=0:           
+            upright=p2 
+    if a==2:#left
+        if grill[c][d-1][0]==0 :
+            reset=1
+        if grill[c+1][d][0]==0 and grill[c-1][d][0]==0:
+            right=1
+        if grill[c+1][d][0]!=0 and grill[c-1][d][0]==0:
+            right=p1
+        if grill[c+1][d][0]==0 and grill[c-1][d][0]!=0:
+            right=p1
+        if grill[c+1][d][0]!=0 and grill[c-1][d][0]!=0:
+            right= p
+        if grill[c+1][d][0]!=0
+            downright=p2 
+        if grill[c-1][d][0]!=0:          
+            upright=p2 
+        
+    if a==3:#right
+        if grill[c][d+1][0]==0 :
+            reset=1
+        if grill[c+1][d][0]==0 and grill[c-1][d][0]==0:
+            left=1
+        if grill[c+1][d][0]!=0 and grill[c-1][d][0]==0:
+            left=p1
+        if grill[c+1][d][0]==0 and grill[c-1][d][0]!=0:
+            left=p1
+        if grill[c+1][d][0]!=0 and grill[c-1][d][0]!=0:
+            left= p
+        if grill[c+1][d][0]!=0
+            downleft=p2 
+        if grill[c-1][d][0]!=0:          
+            upleft=p2 
+            
+    return up,down,left,right,reset，upleft,upright,downleft,downright
+
+def calculR(grill, i,j,ax,p):
+    c=i+1
+    d=j+1
+    p1=(1+p)/2
+    p2=(1-p)/2
+    if ax==0:#up
+        if(grill[c-1][d][0]==0):
+
+            a=-grill[i][j][0]
+        elif grill[c-1][d-1][0]==0 and grill[c-1][d+1][0]==0:
+
+            a= -grill[c-1][d][0]
+        elif grill[c-1][d-1][0]==0 and grill[c-1][d+1][0]!=0:
+
+            a=-grill[c-1][d][0]*p1-grill[c-1][d+1][0]*p2
+        elif grill[c-1][d-1][0]!=0 and grill[c-1][d+1][0]==0:
+
+            a=-grill[c-1][d][0]*p1-grill[c-1][d-1][0]*p2
+        else: 
+
+            a=-grill[c-1][d][0]*p-grill[c-1][d-1][0]*p2+grill[c-1][d+1][0]*p2
+    if ax==1:#down
+        if(grill[c+1][d][0]==0):
+
+            a=-grill[i][j][0]
+        elif grill[c+1][d-1][0]==0 and grill[c+1][d+1][0]==0:
+
+            a= -grill[c+1][d][0]
+
+        elif grill[c+1][d-1][0]==0 and grill[c+1][d+1][0]!=0:
+
+            a=-grill[c+1][d][0]*p1-grill[c+1][d+1][0]*p2
+
+        elif grill[c+1][d-1][0]!=0 and grill[c+1][d+1][0]==0:
+            a=-grill[c+1][d][0]*p1-grill[c+1][d-1][0]*p2
+        else: 
+            a=-grill[c+1][d][0]*p-grill[c+1][d-1][0]*p2+grill[c+1][d+1][0]*p2
+    if ax==2:#left
+        if(grill[c][d-1][0]==0):
+            
+            a=-grill[i][j][0]
+        elif grill[c+1][d-1][0]==0 and grill[c-1][d-1][0]==0:
+            
+            a= -grill[c][d-1][0]
+        elif grill[c+1][d-1][0]==0 and grill[c-1][d-1][0]!=0:
+            
+            a=-grill[c][d-1][0]*p1-grill[c-1][d-1][0]*p2
+        elif grill[c+1][d-1][0]!=0 and grill[c-1][d-1][0]==0:
+            
+            a=-grill[c][d-1][0]*p1-grill[c+1][d-1][0]*p2
+        else:
+            
+            a=-grill[c][d-1][0]*p-grill[c+1][d-1][0]*p2+grill[c-1][d-1][0]*p2
+        
+    if ax==3:#right
+        if(grill[c][d+1][0]==0):
+            
+            a=-grill[i][j][0]
+        elif grill[c+1][d+1][0]==0 and grill[c-1][d+1][0]==0:
+            
+            a= -grill[c][d+1][0]
+        elif grill[c+1][d+1][0]==0 and grill[c-1][d+1][0]!=0:
+            
+            a=-grill[c][d+1][0]*p1-grill[c-1][d+1][0]*p2
+        elif grill[c+1][d+1][0]!=0 and grill[c-1][d+1][0]==0:
+            
+            a=-grill[c][d+1][0]*p1-grill[c+1][d+1][0]*p2
+        else:
+            
+            a=-grill[c][d+1][0]*p-grill[c+1][d+1][0]*p2+grill[c-1][d+1][0]*p2
+    return a
+
+
+def optimale(n,m,nba,grill,p,gamma):
+    
+    # G : matrice des gains
+   
+
+    nbcont = n*m
+    nbvar = n*m*nba
+
+    # Range of plants and warehouses
+    lignes = range(nbcont)
+    colonnes = range(nbvar)
+
+    # Matrice des contraintes
+    G=[]
+    for i in range(n):
+        for j in range(m):
+            tab=np.zeros(n*m*nba)
+            for a in range(nba):
+                tab[i*m*nba+j*nba+a]+=1
+                if(grill[i+1][j+1][0]!=0):
+                    p,down,left,right,reset，upleft,upright,downleft,downright=calculTransfert(grill,i,j,a,p)
+                    tab[i*m*nba+j*nba+a]+=reset*gamma
+                    if(i>0):
+                        tab[(i-1)*m*nba+j*nba+a]+=up*gamma
+                    if(i<n-1):
+                        tab[(i+1)*m*nba+j*nba+a]+=down*gamma
+                    if(j>0):
+                        tab[i*m*nba+(j-1)*nba+a]+=left*gamma
+                    if(j<n-1):  
+                        tab[i*m*nba+(j+1)*nba+a]+=right*gamma
+                    if(i>0 and j>0):
+                        tab[(i-1)*m*nba+(j-1)*nba+a]+=upleft*gamma
+                    if(i>0 and j<n-1):
+                        tab[(i-1)*m*nba+(j+1)*nba+a]+=upright*gamma
+                    if(i<n-1 and j>0):
+                        tab[(i+1)*m*nba+(j-1)*nba+a]+=downleft*gamma
+                    if(i<n-1 and j<n-1):
+                        tab[(i+1)*m*nba+(j+1)*nba+a]+=downright*gamma
+
+            G.append(tab)
+    G=np.array(G)
+    # Second membre
+    b = []
+    
+    for i in range(n) :
+        for j in range(m):
+            if(grill[i+1][j+1][0]==0):
+                b.append(0)
+            else:
+                b.append(1)
+    # Coefficients de la fonction objectif
+    c = []
+    for i in range(n) :
+        for j in range(m):
+            for a in range(nba):
+                r=calculR(grill, i,j,a,p)
+                c.append(r)
+
+         
+    m1 = Model("mogplex")     
+
+    # declaration variables de decision
+    x = []
+    for i in colonnes:
+        x.append(m1.addVar(vtype=GRB.CONTINUOUS, lb=0, name="x%d" % (i+1)))
+
+    # maj du modele pour integrer les nouvelles variables
+    m1.update()
+
+    obj = LinExpr();
+    obj =0
+    for j in colonnes:
+        obj += c[j] * x[j]
+
+    # definition de l'objectif
+    m1.setObjective(obj,GRB.MAXIMIZE)
+    m1.setParam("OutputFlag",False)
+    # Definition des contraintes
+    for i in lignes:
+        m1.addConstr(quicksum(G[i][j]*x[j] for j in colonnes) == b[i], "Contrainte%d" % i)
+
+    # Resolution
+    m1.optimize()
+
+
+    # print("")                
+    # print('Solution optimale:')
+    
+    # print("")
+    # print('Valeur de la fonction objectif :', m.objVal)
+    # print("dico :",dico_opt)
+    tab=[]
+    for i in range(n):
+        for j in range(m):
+            tab1=[]
+            for a in range(nba):
+                
+                v=x[i*m*nba+j*nba+a].X
+                tab1.append(v)
+            amax=np.argmax(tab1)
+            if(grill[i+1][j+1][0]!=0):
+            v=tab1[amax]
+            if (v>0):
+                for el in tab1:
+                    if el >0 and el!=v:
+                        print(tab1)   
+            tab.append(tab1)
+    
+    return tab
+
+def optimalepure(n,m,nba,grill,p,gamma):
+    
+    # G : matrice des gains
+   
+
+    nbcont = n*m
+    nbvar = n*m*nba
+
+    # Range of plants and warehouses
+    lignes = range(nbcont)
+    colonnes = range(nbvar)
+
+    # Matrice des contraintes
+    G=[]
+    for i in range(n):
+        for j in range(m):
+            tab=np.zeros(n*m*nba)
+            for a in range(nba):
+                tab[i*m*nba+j*nba+a]+=1
+                if(grill[i+1][j+1][0]!=0):
+                    up,down,left,right,reset=calculTransfert(grill,i,j,a,p)
+                    tab[i*m*nba+j*nba+a]+=reset*gamma
+                    if(i>0):
+                        tab[(i-1)*m*nba+j*nba+a]+=up*gamma
+                    if(i<n-1):
+                        tab[(i+1)*m*nba+j*nba+a]+=down*gamma
+                    if(j>0):
+                        tab[i*m*nba+(j-1)*nba+a]+=left*gamma
+                    if(j<n-1):  
+                        tab[i*m*nba+(j-1)*nba+a]+=right*gamma
+            G.append(tab)
+    G=np.array(G)
+    # Second membre
+    b = []
+    
+    for i in range(n) :
+        for j in range(m):
+            if(grill[i+1][j+1][0]==0):
+                b.append(0)
+            else:
+                b.append(1)
+    # Coefficients de la fonction objectif
+    c = []
+    for i in range(n) :
+        for j in range(m):
+            for a in range(nba):
+                print(a)
+                r=calculR(grill, i,j,a,p)
+                c.append(r)
+
+         
+    m1 = Model("mogplex")     
+
+    # declaration variables de decision
+    x = []
+    for i in colonnes:
+        x.append(m1.addVar(vtype=GRB.CONTINUOUS, lb=0, name="x%d" % (i+1)))
+    d=[]
+    tab=[]
+    for i in range(n):
+        for j in range(m): 
+            for a in range(nba): 
+                d.append(m1.addVar(vtype=GRB.BINARY, lb=0,ub=1))
+
+    # maj du modele pour integrer les nouvelles variables
+    m1.update()
+
+    obj = LinExpr();
+    obj =0
+    for j in colonnes:
+        obj += c[j] * x[j]
+
+    # definition de l'objectif
+    m1.setObjective(obj,GRB.MAXIMIZE)
+
+    # Definition des contraintes
+    for i in lignes:
+        m1.addConstr(quicksum(G[i][j]*x[j] for j in colonnes) == b[i], "Contrainte%d" % i)
+    for i in range(n):
+        for j in range(m):
+            m1.addConstr(quicksum(d[i*m*nba+j*nba+a] for a in range(nba)) <= 1, "Contrainte%d" % (i*m+j+nbcont))
+    for i in colonnes:
+        m1.addConstr((1-gamma)*x[i]<= d[i], "Contrainte%d" % (i+n*m+nbcont))
+
+    # Resolution
+    m1.optimize()
+
+
+    # print("")                
+    # print('Solution optimale:')
+    
+    # print("")
+    # print('Valeur de la fonction objectif :', m.objVal)
+    # print("dico :",dico_opt)
+    tab=np.zeros((n,m))
+    for i in range(n):
+        for j in range(m):
+            tab1=[]
+            for a in range(nba):
+                
+                v=x[i*m*nba+j*nba+a].X
+                tab1.append(v)
+            amax=np.argmax(tab1)
+            v=tab1[amax]
+            if (v>0):
+                for el in tab1:
+                    if el >0 and el!=v:
+                        print("error")   
+            tab[i][j]=amax
+
+
+    return tab
